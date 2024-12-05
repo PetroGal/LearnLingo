10b1c84e75ab911753e3c322ebaee0851df3992d
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-05 20:50:56 +0200

    initialized firebase covered App with BrowserRouter

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3Tj9KoWvIyt63HLqWpcNKZG5PT3-k8VA",
+  authDomain: "learnlingo-6df12.firebaseapp.com",
+  projectId: "learnlingo-6df12",
+  storageBucket: "learnlingo-6df12.firebasestorage.app",
+  messagingSenderId: "515029356688",
+  appId: "1:515029356688:web:fbebc70cddb848edc5c815",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
