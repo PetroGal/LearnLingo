59399a52fdce5951c8afd7840c4fc3d24bf7e663
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-06 23:02:41 +0200

    created file of teachers objects array and function for converting array into teachers object

---
+++ src/utils/processTeachers.js
@@ -0,0 +1,9 @@
+import teachers from "./teachersData.js"
+
+export default function processTeachers() {
+  return teachers.reduce((result, teacher) => {
+    const id = crypto.randomUUID()
+    result[id] = { ...teacher, id }
+    return result
+  }, {})
+}
