2da7c0e78c63539f601893843a888b6285c6da13
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-08 13:27:09 +0200

    implemented functionality to get date from the Firebase

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import processTeachers from "../../utils/processTeachers.js"
 import { writeTeachersData } from "../../utils/firebaseDatabase.js"
+import { getTeachersData } from "../../utils/firebaseDatabase.js"
 
 export default function App() {
   const teachers = processTeachers()
 
-  writeTeachersData(teachers)
+  //writeTeachersData(teachers)
+  getTeachersData()
+
   return (
     <div>
       <h1>Welcome to My Language App</h1>
--- src/utils/firebaseDatabase.js
+++ src/utils/firebaseDatabase.js
@@ -1,4 +1,4 @@
-import { ref, set } from "firebase/database"
+import { get, ref, set } from "firebase/database"
 import db from "../firebaseConfig.js"
 
 export function writeTeachersData(teachers) {
@@ -10,5 +10,21 @@ export function writeTeachersData(teachers) {
     })
     .catch((error) => {
       console.error("There is an error when writing data: ", error)
+    })
+}
+
+export function getTeachersData() {
+  const teachersRef = ref(db, "teachers")
+
+  get(teachersRef)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log("Data received: ", snapshot.val())
+      } else {
+        console.log("Data is not found")
+      }
+    })
+    .catch((error) => {
+      console.log("Data is not available: ", error)
     })
 }
