416f7992c1dbc3c2d97cbfb865aa771bd82a38f4
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-09 18:18:27 +0200

    embed Google font code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
+import css from "./Header.module.css"
 
 export default function Header() {
   return (
     <header>
-      <NavLink to='/'>LearnLingo</NavLink>
+      <NavLink to='/' className={css.logo}>
+        LearnLingo
+      </NavLink>
     </header>
   )
 }
