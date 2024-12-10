376a6f1d389de0377727aa27f4052711dab91c37
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-10 19:42:53 +0200

    added and styled logo icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react"
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import css from "./Header.module.css"
 
 export default function Header() {
   return (
     <header>
       <NavLink to='/' className={css.logo}>
+        <svg className={css.logoIcon} width='28' height='28'>
+          <use href='/icons.svg#icon-logo'></use>
+        </svg>
         LearnLingo
       </NavLink>
     </header>
