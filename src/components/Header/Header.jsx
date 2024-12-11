ca1a3de88b015f44249e104bc5800ee7f7570e00
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-11 11:27:37 +0200

    created and styled header navigation links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,12 +5,24 @@ import css from "./Header.module.css"
 export default function Header() {
   return (
     <header>
-      <NavLink to='/' className={css.logo}>
-        <svg className={css.logoIcon} width='28' height='28'>
-          <use href='/icons.svg#icon-logo'></use>
-        </svg>
-        LearnLingo
-      </NavLink>
+      <div className={css.headerContainer}>
+        <div className={css.headerWrapper}>
+          <NavLink to='/' className={css.logo}>
+            <svg className={css.logoIcon} width='28' height='28'>
+              <use href='/icons.svg#icon-logo'></use>
+            </svg>
+            LearnLingo
+          </NavLink>
+          <div className={css.navWrapper}>
+            <NavLink to='/home' className={css.homeLink}>
+              Home
+            </NavLink>
+            <NavLink to='/teachers' className={css.teachersLink}>
+              Teachers
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
