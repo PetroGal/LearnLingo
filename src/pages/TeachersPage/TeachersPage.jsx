22558732d0ed68f71f59c688bf89eafafdebb8d9
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-20 16:23:42 +0200

    added Filters TeachersList and LoadMoreBtn components on TeachersList

--- src/pages/TeachersPage/TeachersPage.jsx
+++ src/pages/TeachersPage/TeachersPage.jsx
@@ -1,7 +1,16 @@
+import Filters from "../../components/Filters/Filters.jsx"
+import LoadMoreBtn from "../../components/LoadMoreBtn/LoadMoreBtn.jsx"
+import TeachersList from "../../components/TeachersList/TeachersList.jsx"
+import css from "./TeachersPage.module.css"
+
 export default function TeachersPage() {
   return (
-    <div>
-      <h1>Teachers page</h1>
+    <div className={css.teachersSection}>
+      <div className={css.teachersContainer}>
+        <Filters />
+        <TeachersList />
+        <LoadMoreBtn />
+      </div>
     </div>
   )
 }
