b13ec3765302eaf8c232288a9d7a701b71080f59
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-21 17:18:28 +0200

    added initial Filters component elements

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,3 +1,36 @@
+import css from "./Filters.module.css"
+
 export default function Filters() {
-  return <div>This is Filter component</div>
+  return (
+    <div>
+      <ul className={css.filtersList}>
+        <li className={`${css.filterItem} ${css.filterLanguage}`}>
+          <div className={css.filterItemsWrapper}>
+            <label htmlFor='languages'>Languages</label>
+            <select name='' id=''>
+              <option value='french'>French</option>
+              <option value='english'>English</option>
+              <option value='spain'>Spain</option>
+            </select>
+          </div>
+        </li>
+        <li className={`${css.filterItem} ${css.filterLevel}`}>
+          <label htmlFor='levels'>Level of knowledge</label>
+          <select name='' id=''>
+            <option value=''>A1 Beginner</option>
+            <option value=''>A1 Beginner</option>
+            <option value=''>A1 Beginner</option>
+          </select>
+        </li>
+        <li className={`${css.filterItem} ${css.filterPrice}`}>
+          <label htmlFor='prices'>Price</label>
+          <select name='' id=''>
+            <option value=''>30 $</option>
+            <option value=''>30 $</option>
+            <option value=''>30 $</option>
+          </select>
+        </li>
+      </ul>
+    </div>
+  )
 }
