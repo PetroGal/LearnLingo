11ea4b0596ae26a41910640a4a030919b6d664e5
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-12-23 20:34:07 +0200

    styled filter elements removed default chevron-down icons

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -4,31 +4,43 @@ export default function Filters() {
   return (
     <div>
       <ul className={css.filtersList}>
-        <li className={`${css.filterItem} ${css.filterLanguage}`}>
+        <li className={css.filterItem}>
           <div className={css.filterItemsWrapper}>
-            <label htmlFor='languages'>Languages</label>
-            <select name='' id=''>
-              <option value='french'>French</option>
-              <option value='english'>English</option>
-              <option value='spain'>Spain</option>
+            <label htmlFor='language'>Languages</label>
+            <select
+              name='language'
+              id='language'
+              className={css.filterLanguage}
+            >
+              <option value='French'>French</option>
+              <option value='English'>English</option>
+              <option value='German'>German</option>
+              <option value='Ukrainian'>Ukrainian</option>
+              <option value='Polish'>Polish</option>
             </select>
           </div>
         </li>
-        <li className={`${css.filterItem} ${css.filterLevel}`}>
-          <label htmlFor='levels'>Level of knowledge</label>
-          <select name='' id=''>
-            <option value=''>A1 Beginner</option>
-            <option value=''>A1 Beginner</option>
-            <option value=''>A1 Beginner</option>
-          </select>
+        <li className={css.filterItem}>
+          <div className={css.filterItemsWrapper}>
+            <label htmlFor='level'>Level of knowledge</label>
+            <select name='level' id='level' className={css.filterLevel}>
+              <option value='beginner'>A1 Beginner</option>
+              <option value='elementary'>A2 Elementary</option>
+              <option value='intermediate'>B1 Intermediate</option>
+              <option value='upper-intermediate'>B2 Upper-Intermediate</option>
+            </select>
+          </div>
         </li>
-        <li className={`${css.filterItem} ${css.filterPrice}`}>
-          <label htmlFor='prices'>Price</label>
-          <select name='' id=''>
-            <option value=''>30 $</option>
-            <option value=''>30 $</option>
-            <option value=''>30 $</option>
-          </select>
+        <li className={css.filterItem}>
+          <div className={css.filterItemsWrapper}>
+            <label htmlFor='price'>Price</label>
+            <select name='price' id='price' className={css.filterPrice}>
+              <option value='10'>10 $</option>
+              <option value='20'>20 $</option>
+              <option value='30'>30 $</option>
+              <option value='40'>40 $</option>
+            </select>
+          </div>
         </li>
       </ul>
     </div>
