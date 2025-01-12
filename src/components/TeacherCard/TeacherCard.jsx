f7c31ae2d64ae808cff6fd2f64e2c5a03fc515dc
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-01-12 22:06:38 +0200

    initial creation and styling of TeacherCard component

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -1,59 +1,101 @@
 import React from "react"
+import teachers from "../../utils/teachersData.js"
 import css from "./TeacherCard.module.css"
 
-export default function TeacherCard({
-  name,
-  surname,
-  languages,
-  rating,
-  lessons_done,
-  price_per_hour,
-  avatar_url,
-  lesson_info,
-  conditions,
-  levels,
-}) {
+// export default function TeacherCard({
+//   name,
+//   surname,
+//   languages,
+//   rating,
+//   lessons_done,
+//   price_per_hour,
+//   avatar_url,
+//   lesson_info,
+//   conditions,
+//   levels,
+// }) {
+//   return (
+//     <div className={css.teacherCard}>
+//       <div className={css.avatarWrapper}>
+//         <img
+//           className={css.avatar}
+//           src={avatar_url}
+//           alt={`${name} ${surname}`}
+//         />
+//       </div>
+
+//       <div className={css.teacherInfo}>
+//         <h3>
+//           {name} {surname}
+//         </h3>
+//         <div className={css.teacherDetails}>
+//           <p>Languages: {languages}</p>
+//           <p>Rating: {rating} ⭐</p>
+//           <p>Lessons Done: {lessons_done}</p>
+//           <p>Price / hour: ${price_per_hour}</p>
+//         </div>
+
+//         <div className={css.lessonInfo}>
+//           <p>{lesson_info}</p>
+//         </div>
+
+//         <div className={css.conditions}>
+//           {conditions.map((condition, index) => (
+//             <p key={index}>{condition}</p>
+//           ))}
+//         </div>
+
+//         <div className={css.levels}>
+//           {levels.map((level, index) => (
+//             <span key={index} className={css.levelTag}>
+//               #{level}
+//             </span>
+//           ))}
+//         </div>
+
+//         <button className={css.readMoreBtn}>Read more</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+export default function TeacherCard({ teachers }) {
   return (
-    <div className={css.teacherCard}>
-      <div className={css.avatarWrapper}>
-        <img
-          className={css.avatar}
-          src={avatar_url}
-          alt={`${name} ${surname}`}
-        />
+    <>
+      <div>
+        <div className={css.cardWrapper}>
+          <div className={css.avatarWrapper}>
+            <img
+              src={teachers[0].avatar_url}
+              className={css.avatarImg}
+              alt={teachers[0].name}
+            />
+          </div>
+          <div className={css.teachersDetailsWrapper}>
+            <div className={css.teacherRates}>
+              <p>Languages</p>
+              <p>Lessons online</p>
+              <p>Lessons done: {teachers[0].lessons_done}</p>
+              <p> Rating: {teachers[0].rating}</p>
+              <p>Price / 1 hour: {teachers[0].price_per_hour} $</p>
+            </div>
+
+            <div className={css.teachersInfo}>
+              <h3>
+                {teachers[0].name} {teachers[0].surname}
+              </h3>
+              <p>Speaks: {teachers[0].languages.join(", ")}</p>
+              <p>Lesson info: {teachers[0].lesson_info}</p>
+              <p>Conditions: {teachers[0].conditions}</p>
+              <button className={css.readMoreBtn}>Read More</button>
+            </div>
+          </div>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
       </div>
-
-      <div className={css.teacherInfo}>
-        <h3>
-          {name} {surname}
-        </h3>
-        <div className={css.teacherDetails}>
-          <p>Languages: {languages}</p>
-          <p>Rating: {rating} ⭐</p>
-          <p>Lessons Done: {lessons_done}</p>
-          <p>Price / hour: ${price_per_hour}</p>
-        </div>
-
-        <div className={css.lessonInfo}>
-          <p>{lesson_info}</p>
-        </div>
-
-        <div className={css.conditions}>
-          {conditions.map((condition, index) => (
-            <p key={index}>{condition}</p>
-          ))}
-        </div>
-
-        <div className={css.levels}>
-          {levels.map((level, index) => (
-            <span key={index} className={css.levelTag}>
-              #{level}
-            </span>
-          ))}
-        </div>
-
-        <button className={css.readMoreBtn}>Read more</button>
-      </div>
-    </div>
+    </>
   )
 }
--- src/components/TeachersList/TeachersList.jsx
+++ src/components/TeachersList/TeachersList.jsx
@@ -1,12 +1,11 @@
 import TeacherCard from "../TeacherCard/TeacherCard.jsx"
 import teachers from "../../utils/teachersData.js"
+import css from "./TeachersList.module.css"
 
 export default function TeachersList() {
   return (
-    <div>
+    <ul className={css.teachersList}>
       <TeacherCard teachers={teachers} />
-      <TeacherCard />
-      <TeacherCard />
-    </div>
+    </ul>
   )
 }
