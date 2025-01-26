8b45fb114c116f5901a238762cca1f3f866a0a22
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-01-26 20:06:58 +0200

    handled dynamic data in Filters component

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,6 +1,86 @@
 import css from "./Filters.module.css"
+import { useState } from "react"
+
+// export default function Filters() {
+//   return (
+//     <div>
+//       <ul className={css.filtersList}>
+//         <li className={css.filterItem}>
+//           <div className={css.filterItemsWrapper}>
+//             <label htmlFor='language'>Languages</label>
+//             <div className={css.languageSelectWrap}>
+//               <select
+//                 name='language'
+//                 id='language'
+//                 className={css.filterLanguage}
+//               >
+//                 <option value='French'>French</option>
+//                 <option value='English'>English</option>
+//                 <option value='German'>German</option>
+//                 <option value='Ukrainian'>Ukrainian</option>
+//                 <option value='Polish'>Polish</option>
+//               </select>
+//               <svg
+//                 className={css.chevronDownLanguageIcon}
+//                 width='20px'
+//                 height='20px'
+//               >
+//                 <use href='/public/icons.svg#icon-chevron-downOpt'></use>
+//               </svg>
+//             </div>
+//           </div>
+//         </li>
+//         <li className={css.filterItem}>
+//           <div className={css.filterItemsWrapper}>
+//             <label htmlFor='level'>Level of knowledge</label>
+//             <div className={css.levelSelectWrap}>
+//               <select name='level' id='level' className={css.filterLevel}>
+//                 <option value='beginner'>A1 Beginner</option>
+//                 <option value='elementary'>A2 Elementary</option>
+//                 <option value='intermediate'>B1 Intermediate</option>
+//                 <option value='upper-intermediate'>
+//                   B2 Upper-Intermediate
+//                 </option>
+//               </select>
+//               <svg
+//                 className={css.chevronDownLevelIcon}
+//                 width='20px'
+//                 height='20px'
+//               >
+//                 <use href='/public/icons.svg#icon-chevron-downOpt'></use>
+//               </svg>
+//             </div>
+//           </div>
+//         </li>
+//         <li className={css.filterItem}>
+//           <div className={css.filterItemsWrapper}>
+//             <label htmlFor='price'>Price</label>
+//             <div className={css.priceSelectWrap}>
+//               <select name='price' id='price' className={css.filterPrice}>
+//                 <option value='10'>10 $</option>
+//                 <option value='20'>20 $</option>
+//                 <option value='30'>30 $</option>
+//                 <option value='40'>40 $</option>
+//               </select>
+//               <svg
+//                 className={css.chevronDownPriceIcon}
+//                 width='20px'
+//                 height='20px'
+//               >
+//                 <use href='/public/icons.svg#icon-chevron-downOpt'></use>
+//               </svg>
+//             </div>
+//           </div>
+//         </li>
+//       </ul>
+//     </div>
+//   )
+// }
 
 export default function Filters() {
+  const [selectedLanguage, setSelectedLanguage] = useState("")
+  const [selectedLevel, setSelectedLevel] = useState("")
+  const [selectedPrice, setSelectedPrice] = useState("")
   return (
     <div>
       <ul className={css.filtersList}>
@@ -12,6 +92,8 @@ export default function Filters() {
                 name='language'
                 id='language'
                 className={css.filterLanguage}
+                value={selectedLanguage}
+                onChange={(e) => setSelectedLanguage(e.target.value)}
               >
                 <option value='French'>French</option>
                 <option value='English'>English</option>
@@ -33,7 +115,13 @@ export default function Filters() {
           <div className={css.filterItemsWrapper}>
             <label htmlFor='level'>Level of knowledge</label>
             <div className={css.levelSelectWrap}>
-              <select name='level' id='level' className={css.filterLevel}>
+              <select
+                name='level'
+                id='level'
+                className={css.filterLevel}
+                value={selectedLevel}
+                onChange={(e) => setSelectedLevel(e.target.value)}
+              >
                 <option value='beginner'>A1 Beginner</option>
                 <option value='elementary'>A2 Elementary</option>
                 <option value='intermediate'>B1 Intermediate</option>
@@ -55,7 +143,13 @@ export default function Filters() {
           <div className={css.filterItemsWrapper}>
             <label htmlFor='price'>Price</label>
             <div className={css.priceSelectWrap}>
-              <select name='price' id='price' className={css.filterPrice}>
+              <select
+                name='price'
+                id='price'
+                className={css.filterPrice}
+                value={selectedPrice}
+                onChange={(e) => setSelectedPrice(e.target.value)}
+              >
                 <option value='10'>10 $</option>
                 <option value='20'>20 $</option>
                 <option value='30'>30 $</option>
--- src/components/TeachersList/TeachersList.jsx
+++ src/components/TeachersList/TeachersList.jsx
@@ -2,39 +2,12 @@ import TeacherCard from "../TeacherCard/TeacherCard.jsx"
 import teachers from "../../utils/teachersData.js"
 import css from "./TeachersList.module.css"
 
-// export default function TeachersList() {
-//   return (
-//     <ul className={css.teachersList}>
-//       {teachers.map((teacher) => (
-//         <TeacherCard key={teachers.name + teachers.surname} {...teacher} />
-//       ))}
-//     </ul>
-//   )
-// }
-
 export default function TeachersList() {
-  const [visibleCards, setVisibleCards] = useState(3) // Initially show 3 cards
-
-  // Function to load more cards
-  const loadMoreCards = () => {
-    setVisibleCards((prevVisible) => prevVisible + 3)
-  }
-
   return (
-    <div>
-      <div className={css.teachersList}>
-        {/* Slice the teachers array to only show the visibleCards */}
-        {teachers.slice(0, visibleCards).map((teacher, index) => (
-          <TeacherCard key={teacher.id || index} {...teacher} />
-        ))}
-      </div>
-
-      {/* Show the "Load More" button only if there are more cards to load */}
-      {visibleCards < teachers.length && (
-        <button className={css.loadMoreBtn} onClick={loadMoreCards}>
-          Load More
-        </button>
-      )}
-    </div>
+    <ul className={css.teachersList}>
+      {teachers.map((teacher) => (
+        <TeacherCard key={teachers.name + teachers.surname} {...teacher} />
+      ))}
+    </ul>
   )
 }
