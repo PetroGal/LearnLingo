077afc08b2661eee09fe10bc639eec04e65a93a2
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-03-06 20:46:40 +0200

    positioned close icone placed registerForm inside the modal

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -13,20 +13,45 @@ export default function RegisterForm({ onClose }) {
   }
 
   return (
-    <div>
-      <button type='button' className={css.modalOpenBtn}>
-        <svg className={css.closeIcon} width='32px' height='32px'>
-          <use href='/icons.svg#icon-closeNewOpt'></use>
-        </svg>
-      </button>
-      <h2 className={css.RegistrationTitle}>Registration</h2>
-      <p>
-        Thank you for your interest in our platform! In order to register, we
-        need some information. Please provide us with the following information
-      </p>
-      <input type='text' />
-      <input type='email' />
-      <input type='password' />
+    <div className={css.registerContainer}>
+      <div className={css.registerContent}>
+        <button type='button' className={css.modalCloseBtn}>
+          <svg className={css.closeIcon} width='32px' height='32px'>
+            <use href='/icons.svg#icon-closeNewOpt'></use>
+          </svg>
+        </button>
+        <h2 className={css.registerTitle}>Registration</h2>
+        <p className={css.registerText}>
+          Thank you for your interest in our platform! In order to register, we
+          need some information. Please provide us with the following
+          information
+        </p>
+        <form className={css.registerForm}>
+          <div className={css.registerInputWrap}>
+            <input
+              type='text'
+              className={css.registerInput}
+              {...register("name", { required: "Name is required" })}
+              placeholder='Name'
+            />
+            {errors.name && <p>{errors.name.message}</p>}
+            <input
+              type='text'
+              className={css.registerInput}
+              {...register("name", { required: "Name is required" })}
+              placeholder='Name'
+            />
+            {errors.name && <p>{errors.name.message}</p>}
+            <input
+              type='text'
+              className={css.registerInput}
+              {...register("name", { required: "Name is required" })}
+              placeholder='Name'
+            />
+            {errors.name && <p>{errors.name.message}</p>}
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
