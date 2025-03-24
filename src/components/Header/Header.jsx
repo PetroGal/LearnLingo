41b3ceaf779d264294b94e3c58b0a72523cbb90a
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-03-24 20:10:39 +0200

    fetched user and user name showed use name and Logout in the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react"
+import { useAuth } from "../../context/AuthContext.jsx"
 import { NavLink } from "react-router-dom"
 import css from "./Header.module.css"
 
 export default function Header({ setIsModalOpen, setActiveForm }) {
+  const { user, userName, logout } = useAuth() // Додаємо отримання user і userName
+
   const handleLogin = () => {
     setActiveForm("login")
     setIsModalOpen(true)
@@ -12,6 +15,7 @@ export default function Header({ setIsModalOpen, setActiveForm }) {
     setActiveForm("register")
     setIsModalOpen(true)
   }
+
   return (
     <header className={css.header}>
       <div className={css.headerContainer}>
@@ -32,25 +36,40 @@ export default function Header({ setIsModalOpen, setActiveForm }) {
               </NavLink>
             </div>
             <div className={css.loginWrapper}>
-              <div className={css.loginButtons}>
-                <button
-                  type='button'
-                  className={css.loginBtn}
-                  onClick={handleLogin}
-                >
-                  <svg className={css.loginIcon} width='20px' height='20px'>
-                    <use href='/icons.svg#icon-logIn_opt'></use>
-                  </svg>
-                  <span className={css.loginText}>Log in</span>
-                </button>
-                <button
-                  type='button'
-                  className={css.RegistrationBtn}
-                  onClick={handleRegister}
-                >
-                  Registration
-                </button>
-              </div>
+              {user ? (
+                // Якщо користувач залогінений
+                <div className={css.userSection}>
+                  <span className={css.userName}>👤 {userName}</span>
+                  <button
+                    type='button'
+                    className={css.logoutBtn}
+                    onClick={logout}
+                  >
+                    Logout
+                  </button>
+                </div>
+              ) : (
+                // Якщо користувача немає, показуємо кнопки входу і реєстрації
+                <div className={css.loginButtons}>
+                  <button
+                    type='button'
+                    className={css.loginBtn}
+                    onClick={handleLogin}
+                  >
+                    <svg className={css.loginIcon} width='20px' height='20px'>
+                      <use href='/icons.svg#icon-logIn_opt'></use>
+                    </svg>
+                    <span className={css.loginText}>Log in</span>
+                  </button>
+                  <button
+                    type='button'
+                    className={css.RegistrationBtn}
+                    onClick={handleRegister}
+                  >
+                    Registration
+                  </button>
+                </div>
+              )}
             </div>
           </div>
         </div>
